a511bd3f86e2fe08d8e7e512400640a74c2321a5
doniatung <doniatung32@gmail.com>
2018-03-08 13:20:07 -0500

    woo

--- object.js
+++ object.js
@@ -1,9 +1,8 @@
-//Donia Tung
+//Team acDC
+//Carol Pan & Donia Tung
 //SoftDev2 pd7
-//K#09 -- Ask Ciricles | Change || Die
-//2018-03-06
-
-
+//K#10 -- Objectification
+//2018-03-08
 
 
 var svg = document.getElementById("vimage");
@@ -12,27 +11,37 @@ var clearSVG = document.getElementById("clear");
 var shape = 0;
 var xpos, ypos;
 
-var ballObj = drawDot(50,50,15);
-
-var clicked = function(e){
-  if (e.toElement == this){
-    xpos = e.offsetX;
-    ypos = e.offsetY;
-      drawDot(xpos, ypos, 30);
-  }
-};
-
-var drawDot = function(x,y,size){
+var drawDot = function(x,y){
   var newthing = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   newthing.setAttribute("cx", x);
   newthing.setAttribute("cy", y);
-  newthing.setAttribute("r", size);
-  newthing.setAttribute("fill", "yellow");
+  newthing.setAttribute("r", ballObj.radius);
+  newthing.setAttribute("fill", ballObj.color);
   newthing.setAttribute("stroke", "black");
   newthing.addEventListener("click", change);
   //svg.appendChild(newthing);
   return newthing
 }
+
+var ballObj = {make: drawDot, x: 0; y: 0, color: "red", radius: "30", display: function(){svg.appendChild(ballObj.make(xpos,ypos))},
+ remove: function(obj){svg.removeChild(obj)}, changeX= function(newX){x=newX;}, changeX= function(newY){y=newY;}}
+
+ var changeX = function(x){
+   xpos = x;
+ }
+
+ var changeY = function(y){
+   ypos = y;
+ }
+
+var clicked = function(e){
+  if (e.toElement == this){
+    xpos = e.offsetX;
+    ypos = e.offsetY;
+    //ballObj.make(xpos,ypos);
+    ballObj.display();
+  }
+};
 
 var change = function(e){
   e.preventDefault();
@@ -41,8 +50,10 @@ var change = function(e){
 }
 
 var newbie = function(e){
-   svg.removeChild(this)
-   drawDot( Math.random()*500, Math.random()*500, 30 )
+   ballObj.remove(this);
+   xpos = Math.random()*500;
+   ypos = Math.random()*500;
+   ballObj.display();
 }
 
 var clear = function(e){
