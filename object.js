f120cbfc56e70904cfa2af6f1fc41df3f00d1faf
Valery Chen <cpan3@stuy.edu>
2018-03-08 21:44:03 -0500

    turned into object

--- object.js
+++ object.js
@@ -8,53 +8,64 @@
 var svg = document.getElementById("vimage");
 var clearSVG = document.getElementById("clear");
 
-var shape = 0;
-var xpos, ypos;
-
-var drawDot = function(x,y){
-  var newthing = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  newthing.setAttribute("cx", x);
-  newthing.setAttribute("cy", y);
-  newthing.setAttribute("r", ballObj.radius);
-  newthing.setAttribute("fill", ballObj.color);
-  newthing.setAttribute("stroke", "black");
-  newthing.addEventListener("click", change);
-  //svg.appendChild(newthing);
-  return newthing
+var drawDot = function(){
+    var newthing = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    newthing.setAttribute("cx", 0);
+    newthing.setAttribute("cy", 0);
+    newthing.setAttribute("r", 30);
+    newthing.setAttribute("fill", "red");
+    newthing.setAttribute("stroke", "black");
+    newthing.addEventListener("click", change);
+    //svg.appendChild(newthing);
+    return newthing
 }
 
-var ballObj = {make: drawDot, x: 0; y: 0, color: "red", radius: "30", display: function(){svg.appendChild(ballObj.make(xpos,ypos))},
- remove: function(obj){svg.removeChild(obj)}, changeX= function(newX){x=newX;}, changeX= function(newY){y=newY;}}
-
- var changeX = function(x){
-   xpos = x;
- }
-
- var changeY = function(y){
-   ypos = y;
- }
+function ballObj(){
+    this.x= 0;
+    this.y= 0;
+    this.color= "red";
+    this.radius= "30";
+    this.element= drawDot()
+    this.display= function(){svg.appendChild(this.element)}
+    this.remove= function(){svg.removeChild(this.element);}
+    this.getX= function(){return this.x},
+    this.getY= function(){return this.y;},
+    this.getR= function(){return this.radius;},
+    this.getColor= function(){return this.color;},
+    this.changeX= function(newX){this.x=newX;
+				 this.element.setAttribute("cx", this.x);},
+    this.changeY= function(newY){this.y=newY;
+				 this.element.setAttribute("cy", this.y);},
+    this.changeColor= function(){this.color = "green";
+				 this.element.setAttribute("fill", "green");}
+}
 
 var clicked = function(e){
-  if (e.toElement == this){
-    xpos = e.offsetX;
-    ypos = e.offsetY;
-    //ballObj.make(xpos,ypos);
-    ballObj.display();
-  }
+    e.preventDefault();
+    if (e.target == this){
+	var dot = new ballObj();
+	dot.changeX( e.offsetX);
+	dot.changeY( e.offsetY);
+	//console.log(dot);
+	dot.display();
+	
+    };
 };
 
 var change = function(e){
-  e.preventDefault();
-  this.setAttribute("fill", "green");
-  this.addEventListener("click", newbie)
-}
+    e.preventDefault();
+    //this.changeColor();
+    this.setAttribute("fill", "green");
+    this.addEventListener("click", newbie)
+};
 
 var newbie = function(e){
-   ballObj.remove(this);
-   xpos = Math.random()*500;
-   ypos = Math.random()*500;
-   ballObj.display();
-}
+    svg.removeChild(this);
+    var dot = new ballObj();
+    dot.changeX(Math.random()*500);
+    dot.changeY(Math.random()*500);
+    dot.display();
+};
 
 var clear = function(e){
     while (svg.firstChild) {
